304385dc80066d4b85af112e47059dd8e62f8724
Douglas Michel <douglasmichel.dev@gmail.com>
2025-04-28 21:21:37 -0300

    Aprimora a interface do modal de seleção de tema com ajustes de estilo e funcionalidade

--- js/index.js
+++ js/index.js
@@ -6,34 +6,35 @@ const temaEscuro = document.getElementById('tema-escuro');
 const temaClaro = document.getElementById('tema-claro');
 let temaSelecionado = 'modo-claro';
 
-
+// Abrir modal
 temaBtn.addEventListener('click', () => {
     modal.style.display = 'flex';
 });
 
-
+// Fechar modal sem alterar o tema
 cancelar.addEventListener('click', () => {
     modal.style.display = 'none';
 });
 
-
+// Selecionar tema escuro
 temaEscuro.addEventListener('click', () => {
     temaSelecionado = 'modo-escuro';
     destacarBotao(temaEscuro, temaClaro);
 });
 
-
+// Selecionar tema claro
 temaClaro.addEventListener('click', () => {
     temaSelecionado = 'modo-claro';
     destacarBotao(temaClaro, temaEscuro);
 });
 
-
+// Confirmar seleção do tema
 confirmar.addEventListener('click', () => {
     document.body.className = temaSelecionado;
     modal.style.display = 'none';
 });
 
+// Destacar botão selecionado
 function destacarBotao(botaoSelecionado, botaoNaoSelecionado) {
     botaoSelecionado.classList.add('selecionado');
     botaoNaoSelecionado.classList.remove('selecionado');
